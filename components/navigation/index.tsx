690133bbfe6f4933a445bfc2eeaa8d8fc66f55a2
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-03-09 19:16:48 +0700

    adding tailwind css

---
+++ components/navigation/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPageContext } from 'next';
+
+const Navigation = () => {
+  const test = '';
+  return (
+    <div>
+      <div>aaaa</div>
+    </div>
+  );
+};
+
+export default Navigation;
