42635c2d0481d73ec833db5146d3f144bd54b369
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-04-27 06:36:36 +0700

    repair mui in production build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import "../styles/globals.scss";
 import "../styles/animated.min.scss";
 import type { AppProps } from "next/app";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <StyledEngineProvider injectFirst>
+    <Component {...pageProps} />
+  </StyledEngineProvider>
 );
 
 export default MyApp;
